0057030a1644e173cbfa20d1706157c9fd26b2b1
Vasile Bunacalea <vasile.bunacalea@jivygroup.com>
2018-07-03 17:28:49 +0300

    Add new project connect with  ChannelFactory

---
+++ WCF_HOST_Chan/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using WcfServiceLibrary1;
+
+namespace WCF_HOST_Chan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string data_ac = textBox1.Text;
+            string msc = string.Empty;
+
+            ChannelFactory<IService1> fact = new ChannelFactory<IService1>("client");
+            IService1 channel;
+            channel = fact.CreateChannel();
+            msc = channel.GetData(data_ac);
+
+            MessageBox.Show(msc);
+        }
+    }
+}
